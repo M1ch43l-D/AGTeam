1811f332bb138762c9a0a77c94476fb362be6d7f
Mcnassti <myk.dixon.jr@gmail.com>
2023-12-29 20:34:48 +0000

    Add Dockerfile and update agent configurations

--- first.py
+++ first.py
@@ -13,52 +13,67 @@ config_list_gpt4 = autogen.config_list_from_json(
     },
 )
 gpt4_config = {
-    "cache_seed": 44,
+    "cache_seed": 45,
     "temperature": 0,
     "config_list": config_list_gpt4,
-    "timeout": 120,
+    "timeout": 600,
 }
+
+# Admin
 user_proxy = autogen.UserProxyAgent(
    name="Admin",
-   system_message="A human admin. Interact with the planner to discuss the plan. Plan execution needs to be approved by this admin.",
+   system_message="A human admin.",
    code_execution_config=False,
 )
-engineer = autogen.AssistantAgent(
-    name="Engineer",
+
+# Market Researcher
+market_researcher = autogen.AssistantAgent(
+    name="MarketResearcher",
     llm_config=gpt4_config,
-    system_message="Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor. Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.",
+    system_message="Market Researcher. Analyze market trends, customer needs, and competitor strategies. Brainstorm complementary app concepts as needed. Provide insights to guide the development and positioning of the app.",
 )
-executor = autogen.UserProxyAgent(
-    name="Executor",
-    system_message="Executor. Execute the code written by the engineer and report the result.",
-    human_input_mode="NEVER",
-    code_execution_config={
-        "last_n_messages": 3,
-        "work_dir": "paper",
-        "use_docker": True,
-        },
-)
-scientist = autogen.AssistantAgent(
-    name="Scientist",
+
+# Business Model Architect
+business_model_architect = autogen.AssistantAgent(
+    name="BusinessModelArchitect",
     llm_config=gpt4_config,
-    system_message="Scientist. You follow an approved plan. You are able to categorize papers after seeing their abstracts printed. You don't write code."
+    system_message="Business Model Architect. Design viable business models, develop monetization strategies, and conduct financial forecasting. Align business strategies with market research findings.",
 )
-planner = autogen.AssistantAgent(
-    name="Planner",
+
+# Technical Lead
+technical_lead = autogen.AssistantAgent(
+    name="TechnicalLead",
     llm_config=gpt4_config,
-    system_message="Planner. Suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval. The plan may involve an engineer who can write code and a scientist who doesn't write code. Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.",
+    system_message="Technical Lead. Oversee technical development and advise on technology stack and feasibility. Ensure technical solutions align with business goals and market needs.",
 )
-critic = autogen.AssistantAgent(
-    name="Critic",
+
+# Brand Strategist
+brand_strategist = autogen.AssistantAgent(
+    name="BrandStrategist",
     llm_config=gpt4_config,
-    system_message="Critic. Double check the plan, claims, and code from other agents and provide feedback.",
+    system_message="Brand Strategist. Develop branding and positioning strategies. Create a unique brand identity and messaging that resonates with the target audience.",
 )
-groupchat = autogen.GroupChat(agents=[user_proxy, engineer, scientist, planner, executor, critic], messages=[], max_round=50)
+
+# Creating the group chat with the new team configuration
+groupchat = autogen.GroupChat(
+    agents=[
+        user_proxy, 
+        market_researcher, 
+        business_model_architect, 
+        technical_lead, 
+        brand_strategist
+    ], 
+    messages=[], 
+    max_round=50
+)
+
+# Manager for the group chat
 manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
 
+# Initiating the chat
 user_proxy.initiate_chat(
     manager,
     message="""
-find papers on LLM applications from arxiv in the last week, create a markdown table of different domains.
-""",
+    brainstorm ideas for a cybersecurity Saas app choose the top 3 ideas and progress the ideas 
+    """,
 )
