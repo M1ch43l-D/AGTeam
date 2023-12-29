13431bf297b3edbc4e129cb7495e32de04c3f28d
Mcnassti <myk.dixon.jr@gmail.com>
2023-12-29 22:50:37 +0000

    Update autogenteam.py with gpt-3.5-turbo-instruct model and cache seed 48

--- autogenteam.py
+++ autogenteam.py
@@ -9,11 +9,11 @@ assert os.environ.get("OPENAI_API_KEY")
 config_list_gpt4 = autogen.config_list_from_json(
     "OAI_CONFIG_LIST",
     filter_dict={
-        "model": ["gpt-3.5-turbo", "gpt-4-1106-preview", "gpt-4-32k-v0314"],
+        "model": ["gpt-3.5-turbo", "gpt-3.5-turbo-instruct"],
     },
 )
 gpt4_config = {
-    "cache_seed": 44,
+    "cache_seed": 48,
     "temperature": 0,
     "config_list": config_list_gpt4,
     "timeout": 120,
@@ -26,7 +26,7 @@ user_proxy = autogen.UserProxyAgent(
 engineer = autogen.AssistantAgent(
     name="Engineer",
     llm_config=gpt4_config,
-    system_message="Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor. Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.",
+    system_message="Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor. Don't include multiple code blocks in one response. Do NOT ask others to copy and paste the result. Do not use ANY APIs. Check the execution result returned by the executor. If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.",
 )
 executor = autogen.UserProxyAgent(
     name="Executor",
